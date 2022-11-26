643e540efc6661066dcf27ece5b2b21284d1ab2d
Bhanu Gogia <bhanugogia4@gmail.com>
2022-11-26 18:32:19 +0530

    save token in db

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const BoxSDK = require("box-node-sdk");
 const fetch = require("node-fetch");
+const Pool = require("pg").Pool;
 
 const app = express();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+console.log("Connected to Database!");
 
 app.get("/token", async (req, res) => {
   const body = {
@@ -21,9 +32,16 @@ app.get("/token", async (req, res) => {
   };
   const response = await fetch("https://api.box.com/oauth2/token", options);
   const data = await response.json();
+  pool.query(
+    "UPDATE BOX SET ACCESSTOKEN =($1)",
+    [data.access_token],
+    (err, result) => {
+      if (err) res.status(500).send(err);
+    }
+  );
   res.send(data);
 });
 
 app.listen(8080, () => {
-  console.log("Listening to the server");
+  console.log("Server listening to the requests");
 });
