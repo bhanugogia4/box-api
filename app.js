be0b7c639f140bf6a297d575d0e8b6d16f7ae9d3
Bhanu Gogia <bhanugogia4@gmail.com>
2022-11-29 16:13:00 +0530

    removed unused imports

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const BoxSDK = require("box-node-sdk");
-const fs = require("fs");
 const morgan = require("morgan");
 const authRouter = require("./routes/authRoute");
 const folderRouter = require("./routes/folderRoute");
